37a7669c9052d95cab7cda1c2288803bca129d84
dadulsawol <apriljoydeang23@gmail.com>
2025-05-08 09:06:39 +0800

    Profile Page
    added a new profile page.

--- ai_chatbot/ecommerce_app/urls.py
+++ ai_chatbot/ecommerce_app/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path("add_to_cart/<int:product_id>", views.add_to_cart, name="add_to_cart"),
     path("update_cart/", views.update_cart, name="update_cart"),
     path("remove_from_cart/<int:cart_item_id>", views.remove_from_cart, name="remove_from_cart"),
+    path("profile/",views.profile_page, name="profile"),
 
     path("ordersummary/", views.ordersummary_page, name="ordersummary"),
     path("ordersummary/checkout/", views.checkout_page, name="checkout"),
